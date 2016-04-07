bd2d738a4e504b81cc7bc7dacf7c823ecfabf365
Zac-Taylor <zact@verizon.net>
2016-04-07 15:33:45 -0700

    Program 3

---
+++ SodaExample.py
@@ -0,0 +1,23 @@
+#Liters in a 12 ounce can
+
+Can_Volume = .355
+
+#liters in a two-liter bottle
+
+Bottle_Volume = 2
+
+#Numbers of cans per pack
+
+cans_Per_Pack = 6
+
+#Calculate total volume in cans.
+
+total_Volume = cans_Per_Pack * Can_Volume
+print("a 6 pack of 12 ounce cans contains", total_Volume, "liters.")
+
+ #Calculate total volume in the cans and a 2 litter bottle
+
+total_Volume = total_Volume + Bottle_Volume
+print("A 6 pack and a 2 litter bottle contain", total_Volume, "liters.")
+ 
+ 
