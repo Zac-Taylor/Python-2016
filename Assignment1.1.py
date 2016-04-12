7ac3c1652fe664afce262a59a7315765e141d8e8
Zac-Taylor <zact@verizon.net>
2016-04-12 12:29:01 -0700

    Added First Home Work Assignment

---
+++ Assignment1.1.py
@@ -0,0 +1,18 @@
+BOOK_TAX = 7.5
+SHIPPING_OF_BOOKS = 2.0
+
+values=[]
+
+numOfBooks = input("How mamy books do you have:" + " ")
+numBooks = int(numOfBooks)
+
+for loop in range(numBooks):
+    values.append(float(input("Enter vaule for book:" + " ")))
+
+    mult =1
+    for value in values:
+        mult*=value
+
+total_Costs = (numBooks * SHIPPING_OF_BOOKS) * mult * BOOK_TAX
+
+print(total_Costs)
---
+++ DemoArray.py
@@ -0,0 +1,37 @@
+@@ -0,0 +1,37 @@
+from math import sqrt 
+
+#Declaring two variables
+
+num1 = 123.0
+num2 = 10.0
+
+#Adding the two variables together
+
+totalSum = num1 + num2
+print (totalSum)
+
+#subtracting the two variables together
+
+totalMinus = num1 - num2
+print (totalMinus)
+
+#Multiplying the two variables together
+
+totalMult = num1 * num2
+print(totalMult)
+
+#Dividing the two variables together
+
+totalDiv = num1 / num2
+print(totalDiv)
+
+#Moding the two numbers together
+
+totalMod = num1 % num2
+print("When modding the two numbers you get:",totalMod)
+
+#Squareroot of the first number
+
+totalSqrt = sqrt(num1)
+print("When squarerooting the first number you get:",totalSqrt)
