dd643748f7527cf15524c997e7c27dee6afa887e
Zac-Taylor <zact@verizon.net>
2016-04-25 12:51:17 -0700

    Correct Code

--- Assignment P3.23.py
+++ Assignment P3.23.py
@@ -1,16 +1,15 @@
 #Zachary Taylor's code IST 282
 income = float(input("Please enter your income: "))
 
-if income == 50000 :
-    print("Your income tax is ", income * .01)
-elif income > 50000 and income < 75000:
-    print("Your income tax is ", income * .02)
-elif income > 75000 and income < 1000000:
-    print("Your income tax is ", income * .03)
-elif income >1000000 and income < 250000:
-    print("Your income tax is ", income * .04)
-elif income > 250000 and income < 500000:
-    print("Your income tax is ", income * .05)
+if income == 50000:
+     print ("The tax payable would be: " ,income *0.01)
+elif income <= 75000:
+     print ("The tax payable would be: " ,income *0.02)
+elif income <= 100000:
+     print ("The tax payable would be: " ,income *0.03)
+elif income <= 250000:
+     print ("The tax payable would be: " ,income *0.04)
+elif income <= 5000000:
+     print ("The tax payable would be: " ,income *0.05)
 else:
-    print("Your income tax is ", income * .06)
-    
+    print ("The tax payable would be: " ,income *0.06)
--- Assignment P3.25.py
+++ Assignment P3.25.py
@@ -8,20 +8,20 @@ maritalStatus =input("Please enter s for single, m for married: ")
 #Compute taxes due
 
 if maritalStatus == "s":
-    if income >= 0 and income < 8000 :
+    if income <= 8000 :
         tax1 = income * 0.1 
-    elif income >= 8000 and income < 32000:
-        tax1 = 800 + (income -800) *.15
+    elif income >8000 and income <= 32000:
+        tax1 = 800 + (income -8000) *.15
     else:
-        tax1 = 4400 +(income -4400)* .25
+        tax1 = 4400 +(income -32000)* .25
 
 else:
-    if income >= 0 and income < 16000:
-        tax1 = .1 * income
-    elif income >= 16000 and income < 64000:
-        tax1 = 1600 + (income - 1600) *.15     
+    if income <= 16000:
+        tax1 = income * 0.1
+    elif income >16000 and income <= 64000:
+        tax1 = 1600 + (income - 16000) *.15     
     else:
-        tax1 = 8800 + (income - 8800) *.25
+        tax1 = 8800 + (income - 64000) *.25
 
 
 #Printing the results
