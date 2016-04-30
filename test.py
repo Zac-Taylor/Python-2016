73d0bb5b259a750c1800852a5a658452cd767a72
Zac-Taylor <zact@verizon.net>
2016-04-30 12:35:45 -0700

    test

---
+++ test.py
@@ -0,0 +1,13 @@
+odds = 0
+userInput = input("Please enter a number:")
+
+while userInput != (""):
+    numbers = int(userInput)
+    if numbers > 0 and numbers % 2 ==1:
+        odds= odds + 1
+        print("There were" ,odds, "values)
+    else:
+        userInput = input("Please enter a number:")
+    
+        
+    
